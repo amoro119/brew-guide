09714985b3df271350c62a91d74cf6bb54e1f6d0
chu3 <chuthree01@gmail.com>
2026-04-06 14:12:41 +0800

    优化：设置中的图标均使用图标库

--- src/components/settings/TimerPreview.tsx
+++ src/components/settings/TimerPreview.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import { Pause, RefreshCw } from 'lucide-react';
 import { SettingsOptions } from './Settings';
 import { LayoutSettings } from '@/components/brewing/Timer/Settings';
 
@@ -421,38 +422,18 @@ const TimerPreview: React.FC<TimerPreviewProps> = ({ settings }) => {
               <button
                 className={`${showFlowRate ? 'h-11 w-11 sm:h-12 sm:w-12' : 'h-12 w-12 sm:h-14 sm:w-14'} flex flex-shrink-0 items-center justify-center rounded-full bg-neutral-100 text-neutral-600 dark:bg-neutral-800 dark:text-neutral-400`}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
+                <Pause
                   strokeWidth={1.5}
-                  stroke="currentColor"
                   className={`${showFlowRate ? 'h-4 w-4 sm:h-5 sm:w-5' : 'h-5 w-5 sm:h-6 sm:w-6'}`}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 5.25v13.5m-7.5-13.5v13.5"
-                  />
-                </svg>
+                />
               </button>
               <button
                 className={`${showFlowRate ? 'h-11 w-11 sm:h-12 sm:w-12' : 'h-12 w-12 sm:h-14 sm:w-14'} flex flex-shrink-0 items-center justify-center rounded-full bg-neutral-100 text-neutral-600 dark:bg-neutral-800 dark:text-neutral-400`}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
+                <RefreshCw
                   strokeWidth={1.5}
-                  stroke="currentColor"
                   className={`${showFlowRate ? 'h-4 w-4 sm:h-5 sm:w-5' : 'h-5 w-5 sm:h-6 sm:w-6'}`}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-                  />
-                </svg>
+                />
               </button>
             </div>
           </div>
